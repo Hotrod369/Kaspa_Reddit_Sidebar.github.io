34600ca9cb8b8f02b1e87d488ae0ac06d6e959f4
Hotrod <72291352+Hotrod369@users.noreply.github.com>
2023-04-27 16:22:28 -0400

    Update kaspa-widget.js

--- kaspa-widget.js
+++ kaspa-widget.js
@@ -58,15 +58,7 @@ const fetchData = async () => {
       tooltip: {
         x: {
           format: "dd MMM yyyy HH:mm:ss",
-          formatter: function(val, timestamp) {
-            if (isNaN(timestamp)) {
-              return '';
-            }  
-            const date = new Date(timestamp);
-            const formattedDate = date.toLocaleDateString(undefined, { month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false });
-            return `${formattedDate} UTC`;
-          },
-          colors: "#000000",
+            colors: "#000000",
         },
         y: {
           formatter: function(val) {
