82ac15adfe364369b38903e0a4fe5ed3585ae00b
Hotrod <72291352+Hotrod369@users.noreply.github.com>
2023-05-10 12:59:36 -0400

    Update kaspa-price-widget.py

--- kaspa-price-widget.py
+++ kaspa-price-widget.py
@@ -1,6 +1,7 @@
 import praw
 import os
 import requests
+import time
 
 # Authenticate with Reddit using credentials from GitHub secrets
 reddit = praw.Reddit(
@@ -11,18 +12,39 @@ reddit = praw.Reddit(
     password=os.environ['REDDIT_PASSWORD'],
 )
 
-# Download the HTML file from GitHub
-url = "https://github.com/Hotrod369/Hotrod369.github.io/blob/First/kaspa-price-widget.html"
-response = requests.get(url)
-html_code = response.text
+# Use the URL of your hosted HTML file
+url = "https://hotrod369.github.io/kaspa-price-widget.html"
 
-# Create a new subreddit widget with custom content
-widget = reddit.subreddit("kaspa").widgets.mod.create(
-    "Custom Widget Title",
-    kind="custom",
-    shortName="Kaspa Price Updater",
-    jsonDict={"html": html_code},
-)
+def main():
+    # Get the subreddit object
+    subreddit = reddit.subreddit('kaspa')
 
-# Update the subreddit widget to apply the changes
-widget.mod.update()
+    # Remove any existing Kaspa widget from the sidebar
+    for widget in subreddit.widgets.sidebar:
+        if widget.shortName == 'Kaspa Price Updater':
+            widget.mod.remove()
+            break
+
+    while True:
+        # Download the HTML content from the URL
+        response = requests.get(url)
+        html_code = response.text
+
+        # Create a new subreddit widget with custom content
+        widget = subreddit.widgets.mod.create(
+            "Custom Widget Title",
+            kind="custom",
+            shortName="Kaspa Price Updater",
+            jsonDict={"html": html_code},
+        )
+
+        # Update the subreddit widget to apply the changes
+        widget.mod.update()
+
+        # Wait for 5 minutes before updating again
+        time.sleep(5 * 60)
+
+
+if __name__ == '__main__':
+    main()
+
