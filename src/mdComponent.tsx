8cce8d1ff640546982c970587d5f0d8888136e83
Hotrod <72291352+Hotrod369@users.noreply.github.com>
2023-08-18 02:36:47 -0400

    Added expo router to the project

---
+++ src/mdComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import ReactDOM from 'react-dom'
+
+
+ReactDOM.render(<ReactMarkdown># Kaspa: Building a Fast and Efficient Blockchain Based on Satoshi's Vision
+
+The vision behind this project is to build a Nakamoto-like service that operates as fast as internet speed allows. ...
+
+For Kaspa, we look to silver, which presented a different tradeoff vs. gold. In the original Aramaic text (Dahavavs. Kaspa), silver was historically treated as less precious than gold but more circulative, less valuable yet more acceptable as payment. ...
+
+The consensus stack of Kaspa is designed to address what we believe to be leading challenges for the second decade of crypto. ...
+
+Providing instant confirmation is not a trivial task, less of course one is willing to compromise on the principles of decentralization, or to operate under strong assumptions on the network’s topology and with minimal safety margins. ...
+
+Since transaction ordering is the main challenge of any consensus protocol, Kaspa’s base layer focuses on becoming a fast and scalable transaction sequencing (a.k.a. proof-of-publication) engine. ...
+
+In fact, a rollups-centric Ethereum will fragment the network, hinder composability, and dramatically change the underlying assumptions and dynamics. ...</ReactMarkdown>, document.body)
