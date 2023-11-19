bcfa3f857b72f54d2205247aeaec8dca4ca92647
Hotrod <72291352+Hotrod369@users.noreply.github.com>
2023-11-19 02:50:47 -0500

    Delete src directory

--- src/background.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../assets/images/background.png';
-import './styles.css'; // Import your CSS file
-function BackgroundImage() {
-    return (React.createElement("div", { style: { backgroundImage: `url(${'../assets/images/background.png'})` } }, "Hello World"));
-}
-export default BackgroundImage;
-//# sourceMappingURL=background.js.map
