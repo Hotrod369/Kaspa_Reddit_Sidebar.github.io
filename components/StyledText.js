dd8bcfc91c71276d7889e553396f51f28a05eafc
Hotrod <72291352+Hotrod369@users.noreply.github.com>
2023-11-19 02:50:16 -0500

    Delete components directory

--- components/StyledText.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Text } from './Themed';
-export function MonoText(props) {
-    return React.createElement(Text, { ...props, style: [props.style, { fontFamily: 'SpaceMono' }] });
-}
-//# sourceMappingURL=StyledText.js.map
